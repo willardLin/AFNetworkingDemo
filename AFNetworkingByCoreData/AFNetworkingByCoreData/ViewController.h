f0c8a1bf73cd3258330e15138480f0efaeac8f05
林志利 <lzl@meitu.com>
2014-11-03 14:48:13 +0800

    创建AFNetworking的Demo

---
+++ AFNetworkingByCoreData/AFNetworkingByCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingByCoreData
+//
+//  Created by Willard Lin on 14/11/3.
+//  Copyright (c) 2014年 Meitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
